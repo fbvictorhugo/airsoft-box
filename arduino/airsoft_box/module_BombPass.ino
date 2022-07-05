2227f2e6238880362f89dbe9c1f7d868e2142c8b
Victor Hugo <fb.victorhugo@gmail.com.br>
2022-07-04 21:35:01 -0300

    Atualizando Bomba Pass com timer

--- arduino/airsoft_box/module_BombPass.ino
+++ arduino/airsoft_box/module_BombPass.ino
@@ -1,6 +1,9 @@
 String passA = "";
 String passD = "";
 int DELAY_DEF = 5000;
+String btempoBomba = "";
+long bstarted = 0;
+long bfinished = 0;
 
 void loop_BombPassword() {
 
@@ -13,6 +16,23 @@ void loop_BombPassword() {
   switch (bombState) {
 
     case BOMB_CONFIG:
+      writeLcd("Configure tempo", "");
+      lcd.setCursor(0, 1);
+      lcd.print("Minutos:");
+
+      if (isDigit(key)) {
+        btempoBomba += String(key);
+        writeLcd("", "Minutos: " + String(btempoBomba));
+      } else if (key == KEY_ENTER) {
+        clearDisplay();
+        delay(2000);
+        bombState = BOMB_OFF;
+      } else if (key == KEY_DEL) {
+        btempoBomba = "";
+        writeLcd("Configure tempo", "Minutos:");
+      }
+
+      break;
     case BOMB_OFF:
       writeLcd("Senha p Armar", "");
 
@@ -20,7 +40,7 @@ void loop_BombPassword() {
         passA += String(key);
         writeLcd("Senha p Armar", passA);
 
-      } else if (key == KEY_ENTER || BLUE_READ == LOW || YELLOW_READ == LOW) {
+      } else if (key == KEY_ENTER) {
         writeLcd("Verificando ...", "");
         delay(DELAY_DEF / 2);
         writeLcd(alignText("BOMBA ARMADA", 'C'), " ");
@@ -28,6 +48,8 @@ void loop_BombPassword() {
         showLed(ledBlue, 1);
         bombState = BOMB_ACTIVE;
         delay(DELAY_DEF / 2);
+        bstarted = millis();
+        bfinished = bstarted + (btempoBomba.toInt() * 60000);
       } else if (key == KEY_DEL) {
         passA = "";
         writeLcd("Senha p Armar", " ");
@@ -40,11 +62,17 @@ void loop_BombPassword() {
       showLed(ledBlue, 1);
       writeLcd("Senha p Desarmar", "");
 
+      bstarted = millis();
+      if (bstarted >= bfinished) {
+        bombState = BOMB_EXPLODED;
+        return;
+      }
+
       if (isDigit(key)) {
         passD += String(key);
         writeLcd("Senha p Desarmar", passD);
 
-      } else if (key == KEY_ENTER || BLUE_READ == LOW || YELLOW_READ == LOW) {
+      } else if (key == KEY_ENTER) {
         writeLcd("Verificando ...", "");
         delay(DELAY_DEF);
 
@@ -68,5 +96,10 @@ void loop_BombPassword() {
       showLed(ledYellow, 0);
       showLed(ledBlue, 0);
       break ;
+
+    case BOMB_EXPLODED:
+      writeLcd(alignText("BOMBA", 'C'), alignText("EXPLODIU", 'C'));
+
+      break;
   }
 }
